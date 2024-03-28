81b10b9b127a75516384e635a9701e3d1bf48fa8
SSJROOD <137559489+SSJROOD@users.noreply.github.com>
2024-03-28 16:41:24 -0400

    Update app.py

--- server/api/app.py
+++ server/api/app.py
@@ -8,8 +8,8 @@ CORS(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '355341'
-app.config['MYSQL_DB'] = 'fitnessapp'
+app.config['MYSQL_PASSWORD'] = 'ADDYOUROWNPASSWORD'
+app.config['MYSQL_DB'] = 'ADDDATABASENAME'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
