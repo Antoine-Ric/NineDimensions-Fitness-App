be78e0e5571625c2c01fa82a666f1f5694e1dcc3
SSJROOD <rkv20@fsu.edu>
2024-04-04 15:49:20 -0400

    fixed merged conflict in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/loginpage/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
-import Profile from "./pages/loginpage/profile.jsx"; // Ensure this is capitalized if it's a component
+import Profile from "./pages/loginpage/profile.jsx"; 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route }
 from 'react-router-dom';
@@ -18,24 +18,17 @@ import WelcomeQuiz from './pages/WelcomeQuiz';
 function App() {
   return (
     <Routes>
+      <NavBar />
       <Route path="/" element={<LoginPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path = '/quizintro' element={<QuizIntro />} />
+        <Route path="/welcomequiz" element={<WelcomeQuiz />} />
       {/*add more public routes here*/}
       <Route element={<PrivateRoute />}>
         <Route path="/dashboard/:ID" element={<Dashboard />} />
         <Route path="/profile/:ID" element={<Profile />} />
       </Route>
     </Routes>
-  );
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path = '/quizintro' element={<QuizIntro />} />
-        <Route path="/welcomequiz" element={<WelcomeQuiz />} />
-      </Routes>
-    </Router>
   );
 }
 
