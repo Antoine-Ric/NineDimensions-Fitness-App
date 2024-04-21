510e1aeb2b3b95968c42b00af908d8d40ec6921d
smetllus01 <smetellus01@gmail.com>
2024-04-21 15:45:27 -0400

    Fixed Navbar Look and CSS in LoginPages

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
 function NavBar() {
+  const linkStyle = {
+    position: 'absolute',
+    left: 15,
+    top: 8,
+    fontSize: '2rem',
+    textDecoration: 'none', 
+    color: 'inherit', 
+    cursor: 'default' 
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar" href="/" style={{ position: 'absolute', left: 15, top: 8, fontSize: '2rem' }}>
+        <a href="/" style={linkStyle}>
           NineDimensions
         </a>
-        
       </div>
     </nav>
   );
 }
 
 export default NavBar;
-
